86793954886820c833a546a01e5f4258a84fcea1
Mustapha <mustapha.yinusa@yahoo.com>
2024-05-13 11:36:07 +0100

    Task 100

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,18 +4,22 @@ package { 'nginx':
 }
 
 exec { 'set_hello_world':
-  command => 'echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html'
+  command => 'echo "Hello World!" | sudo tee /var/www/html/index.ngin\
+x-debian.html'
   require => Package['nginx'],
 }
 
 exec { 'set_redirect':
-  command => "sudo sed -i '\\#root /var/www/html;#a\\\\tlocation /redirect_me/ {\\n\\t\\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\\n\\t}' /etc/nginx/sites-available/default",
+  command => "sudo sed -i '\\#root /var/www/html;#a\\\\tlocation /red\
+irect_me/ {\\n\\t\\treturn 301 https://www.youtube.com/watch?v=QH2-TG\
+Ulwu4;\\n\\t}' /etc/nginx/sites-available/default",
   require => require => Package['nginx'],
 }
 
 exec {'HTTP header':
-	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+        command => 'sed -i "25i\        add_header X-Served-By \$host\
+name;" /etc/nginx/sites-available/default',
+        provider => 'shell'
 }
 
 exec { 'restart nginx':
