6abd2829cd3381a780c7172981e6d24e3028e71b
Mustapha <mustapha.yinusa@yahoo.com>
2024-06-17 00:56:07 +0100

    Task 2 fix?

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,13 +9,20 @@ def top_ten(subreddit):
     """
     get the top ten hot topics of a subreddit
     """
-    results = requests.get(
-        f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
-        headers={"User-Agent": "my bot 0.1"})
-    if results.status_code == 200:
-        response_data = results.json()
-        titles = response_data['data']['children']
-        for title in titles:
-            print(title['data']['title'])
-    else:
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "my bot 0.1"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            response_data = response.json()
+            titles = response_data['data']['children']
+            for title in titles:
+                print(title['data']['title'])
+        else:
+            print(None)
+    except requests.RequestException as e:
+        print(None)
+    except ValueError:
         print(None)
