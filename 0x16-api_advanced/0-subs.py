5963d3e266766b1af961c54465fbacdb09cb3bce
Mustapha <mustapha.yinusa@yahoo.com>
2024-06-17 00:41:07 +0100

    Task 1 fix?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,8 +13,12 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        results = response.json().get("data")
+        if results:
+            return results.get("subscribers", 0)
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+except requests.RequestException:
+    return 0
