78d81525c6bacecb373660b2d49b74382667e46f
Mustapha <mustapha.yinusa@yahoo.com>
2024-06-17 00:30:46 +0100

    Task 1 fix?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+Function that queries subscribers on a given Reddit subreddit.
 """
-
 import requests
 
 
@@ -11,11 +10,11 @@ def number_of_subscribers(subreddit):
     Return number of subscribers on a given subreddit.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
