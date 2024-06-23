571d8272b848b6e685ad9822ad9cb093dab397c9
Mustapha <mustapha.yinusa@yahoo.com>
2024-06-23 02:21:48 +0100

    Task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
-# Change OS configuration so that it is possible to login with
-holberton user and open a file without any error message.
+# Change the OS configuration so that it is possible to login with the
+# holberton user and open a file without any error message.
 
 exec {'OS security config':
   command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
